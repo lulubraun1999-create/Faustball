280ad78e0fa8644dbcda16195a25f91a74cb9948
FIstNCISBall <lulubraun1999@gmail.com>
2025-10-22 23:59:09 +0000

    das

--- src/components/main-header.tsx
+++ src/components/main-header.tsx
@@ -50,8 +50,8 @@ export function MainHeader() {
 
   return (
     <header className="sticky top-0 z-50 w-full border-b bg-background/95 backdrop-blur supports-[backdrop-filter]:bg-background/60">
-      <div className="container flex h-16 items-center">
-        <div className="flex flex-1 items-center justify-start">
+      <div className="container flex h-16 items-center justify-center gap-16">
+        <div className="flex items-center">
           <Link href="/dashboard" className="flex items-center space-x-2">
             <span className="font-bold sm:inline-block font-headline">
               TSV Bayer Leverkusen
@@ -59,33 +59,27 @@ export function MainHeader() {
           </Link>
         </div>
 
-        <nav className="flex flex-1 items-center justify-center">
-            <div className="flex items-center space-x-6 text-sm font-medium">
-              <Link
-                href="/dashboard"
-                className="transition-colors hover:text-foreground/80 text-foreground"
-              >
-                Aktuelles
-              </Link>
-              <Link
-                href="/kalender"
-                className="transition-colors hover:text-foreground/80 text-foreground/60"
-              >
-                Kalender
-              </Link>
-              <Link
-                href="/chat"
-                className="transition-colors hover:text-foreground/80 text-foreground/60"
-              >
-                Chat
-              </Link>
-              <VerwaltungDropdown />
-            </div>
+        <nav className="flex items-center justify-center">
+          <div className="flex items-center space-x-6 text-sm font-medium">
+            <Link
+              href="/dashboard"
+              className="transition-colors hover:text-foreground/80 text-foreground"
+            >
+              Aktuelles
+            </Link>
+            <Link
+              href="/chat"
+              className="transition-colors hover:text-foreground/80 text-foreground/60"
+            >
+              Chat
+            </Link>
+            <VerwaltungDropdown />
+          </div>
         </nav>
         
-        <div className="flex flex-1 items-center justify-end space-x-2">
+        <div className="flex items-center justify-end space-x-2">
           <a
-            href="https://www.instagram.com/tsvbayer04faustball"
+            href="https://www.instagram.com"
             target="_blank"
             rel="noopener noreferrer"
           >
