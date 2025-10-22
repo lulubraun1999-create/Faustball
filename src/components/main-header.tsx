e8cd7d17846ed454f71d5851c4cb806f989bd803
FIstNCISBall <lulubraun1999@gmail.com>
2025-10-22 15:53:45 +0000

    wrarum ist das profil nicht weiter rechts

--- src/components/main-header.tsx
+++ src/components/main-header.tsx
@@ -33,16 +33,16 @@ export function MainHeader() {
 
   return (
     <header className="sticky top-0 z-50 w-full border-b bg-background/95 backdrop-blur supports-[backdrop-filter]:bg-background/60">
-      <div className="container flex h-16 items-center">
-        <div className="mr-4 hidden md:flex">
-          <Link href="/dashboard" className="mr-6 flex items-center space-x-2">
+      <div className="container flex h-16 items-center justify-between">
+        <div className="flex items-center space-x-4">
+          <Link href="/dashboard" className="flex items-center space-x-2">
             <span className="font-bold sm:inline-block font-headline">
               TSV Bayer Leverkusen
             </span>
           </Link>
         </div>
         
-        <div className="flex flex-1 items-center justify-end space-x-6">
+        <div className="flex flex-1 items-center justify-end space-x-4">
           <nav className="hidden md:flex items-center space-x-6 text-sm font-medium">
             <Link
               href="/kalender"
@@ -97,7 +97,7 @@ export function MainHeader() {
               rel="noopener noreferrer"
             >
               <Button variant="ghost" size="icon">
-                <Instagram className="h-5 w-5 text-foreground/60 hover:text-foreground/80" />
+                <Instagram className="h-5 w-5 text-foreground/60 hover:text-foreground/80 dark:text-foreground/80 dark:hover:text-foreground" />
                 <span className="sr-only">Instagram</span>
               </Button>
             </a>
