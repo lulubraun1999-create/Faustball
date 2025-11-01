d7e679f2459a678e8f71ca7729d411826367afe5
FIstNCISBall <lulubraun1999@gmail.com>
2025-11-01 22:55:19 +0000

    Try fixing this error: `Runtime ReferenceError: useEffect is not defined

--- src/app/(main)/kalender/page.tsx
+++ src/app/(main)/kalender/page.tsx
@@ -1,7 +1,7 @@
 
 'use client';
 
-import { useState, useMemo, useCallback } from 'react';
+import { useState, useMemo, useCallback, useEffect } from 'react';
 import {
   useFirestore,
   useCollection,
