3092e6d38a6da5b44fe2c16a2c6b0d8f1bc437cb
FIstNCISBall <lulubraun1999@gmail.com>
2025-11-11 01:49:14 +0000

    Try fixing this error: `Runtime Error: Invalid src prop (https://www.tsv

--- next.config.ts
+++ next.config.ts
@@ -1,5 +1,17 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
+  images: {
+    remotePatterns: [
+      {
+        protocol: 'https',
+        hostname: 'www.tsvbayer04.de',
+      },
+      {
+        protocol: 'https',
+        hostname: 'images.unsplash.com',
+      },
+    ],
+  },
 };
 
 export default nextConfig;
